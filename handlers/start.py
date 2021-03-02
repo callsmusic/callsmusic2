2cba0ce40e7b559e26b2675baacb70f24b7ebd09
Roj Serbest <rojserbest@gmail.com>
2021-03-02 17:33:53 +0300

    Add private start command

--- handlers/start.py
+++ handlers/start.py
@@ -4,6 +4,38 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(
     filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        """👋🏻 Hi {message.from_user.first_name}!
+
+I am SU Music Player, an open-source bot that lets you play music in your Telegram groups.
+
+Use the buttons below to know more about me.""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚒ Source code", url="https://t.me/SuMusicPlayer"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/su_Chats"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel 🔈", url="https://t.me/su_Bots"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(
+    filters.command("start")
     & filters.group
     & ~ filters.edited
 )
