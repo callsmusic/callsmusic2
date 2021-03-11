1f948593230471955525495e7d0584e4bb5fa076
KangPaste <80400219+KangPaste@users.noreply.github.com>
2021-03-11 12:07:51 +0700

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def search(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="𝑻𝒖𝒍𝒊𝒔 𝑵𝒂𝒎𝒂 𝑳𝒂𝒈𝒖 𝒀𝒂𝒏𝒈 𝑰𝒏𝒈𝒊𝒏 𝑲𝒂𝒎𝒖 𝑪𝒂𝒓𝒊...",
             switch_pm_parameter="help",
             cache_time=0
         )
@@ -32,7 +32,7 @@ async def search(client: Client, query: InlineQuery):
                         v["viewCount"]["short"]
                     ),
                     input_message_content=InputTextMessageContent(
-                        "https://www.youtube.com/watch?v={}".format(
+                        "/play https://www.youtube.com/watch?v={}".format(
                             v["id"]
                         )
                     ),
@@ -49,6 +49,6 @@ async def search(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Error: Waktu habis!",
                 switch_pm_parameter="",
             )
